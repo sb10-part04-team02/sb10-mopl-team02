a3a89238c6032f6d28b3737935db181fd1cddab5
raonPsm <s102075@naver.com>
2026-05-19 13:21:09 +0900

    chore: initial commit

---
+++ src/test/java/com/team02/mopl/MoplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team02.mopl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoplApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
