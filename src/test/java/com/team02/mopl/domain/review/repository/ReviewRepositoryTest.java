681cb9dd93d1aba244e0aeb659d9b4a5b37b6e73
raonPsm <s102075@naver.com>
2026-06-24 16:48:54 +0900

    fix: ReviewRepositoryTest ContentType import 경로 수정

--- src/test/java/com/team02/mopl/domain/review/repository/ReviewRepositoryTest.java
+++ src/test/java/com/team02/mopl/domain/review/repository/ReviewRepositoryTest.java
@@ -3,8 +3,8 @@ package com.team02.mopl.domain.review.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import com.team02.mopl.domain.content.ContentType;
 import com.team02.mopl.domain.content.entity.Content;
+import com.team02.mopl.domain.content.enums.ContentType;
 import com.team02.mopl.domain.review.entity.Review;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
