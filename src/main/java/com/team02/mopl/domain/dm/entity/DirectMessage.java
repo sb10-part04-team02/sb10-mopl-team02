21e3dff217d464b719475f80580f630dfb493bf6
초시맥북 <fyg7575@gmail.com>
2026-06-24 11:03:47 +0900

    feat: sender, receiver 타입 변경

--- src/main/java/com/team02/mopl/domain/dm/entity/DirectMessage.java
+++ src/main/java/com/team02/mopl/domain/dm/entity/DirectMessage.java
@@ -1,11 +1,14 @@
 package com.team02.mopl.domain.dm.entity;
 
+import static jakarta.persistence.FetchType.LAZY;
+
 import com.team02.mopl.domain.user.entity.User;
 import com.team02.mopl.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinColumns;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -18,17 +21,43 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class DirectMessage extends BaseEntity {
 
-  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  @ManyToOne(fetch = LAZY, optional = false)
   @JoinColumn(name = "conversation_id", columnDefinition = "uuid")
   private Conversation conversation;
 
-  @ManyToOne(fetch = FetchType.LAZY, optional = false)
-  @JoinColumn(name = "sender_id", columnDefinition = "uuid")
-  private User sender;
+  @ManyToOne(fetch = LAZY)
+  @JoinColumns({
+      @JoinColumn(
+          name = "conversation_id",
+          referencedColumnName = "conversation_id",
+          insertable = false,
+          updatable = false
+      ),
+      @JoinColumn(
+          name = "sender_id",
+          referencedColumnName = "member_id",
+          insertable = false,
+          updatable = false
+      )
+  })
+  private ConversationMember sender;
 
-  @ManyToOne(fetch = FetchType.LAZY, optional = false)
-  @JoinColumn(name = "receiver_id", columnDefinition = "uuid")
-  private User receiver;
+  @ManyToOne(fetch = LAZY)
+  @JoinColumns({
+      @JoinColumn(
+          name = "conversation_id",
+          referencedColumnName = "conversation_id",
+          insertable = false,
+          updatable = false
+      ),
+      @JoinColumn(
+          name = "receiver_id",
+          referencedColumnName = "member_id",
+          insertable = false,
+          updatable = false
+      )
+  })
+  private ConversationMember receiver;
 
   @Column(nullable = false, columnDefinition = "TEXT")
   private String content;
