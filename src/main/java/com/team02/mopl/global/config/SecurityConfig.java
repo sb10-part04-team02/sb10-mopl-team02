388c4221da5a0f8df2fb130ecb08b0bb0212841e
Amperisk9 <amperisk9@gmail.com>
2026-06-24 14:04:41 +0900

    feat: CSRF 토큰 조회 API 추가

---
+++ src/main/java/com/team02/mopl/global/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.team02.mopl.global.config;
+
+import com.team02.mopl.global.config.auth.handler.SpaCsrfTokenRequestHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    return http.csrf(
+            csrf ->
+                csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                    .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler()))
+        .authorizeHttpRequests(
+            auth ->
+                auth
+                    // 예외 URL
+                    .requestMatchers("/api/auth/csrf-token")
+                    .permitAll()
+                    // 임시 전체 허용
+                    .anyRequest()
+                    .permitAll())
+        .build();
+  }
+}
