4cfb7164294d70e4e5b3169ac30679fcce07e2da
Amperisk9 <amperisk9@gmail.com>
2026-06-24 17:48:18 +0900

    fix: 예외 url 추가

--- src/main/java/com/team02/mopl/global/config/SecurityConfig.java
+++ src/main/java/com/team02/mopl/global/config/SecurityConfig.java
@@ -8,6 +8,9 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
+import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
 @EnableMethodSecurity
@@ -15,6 +18,9 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    RequestMatcher apiMatcher = PathPatternRequestMatcher.withDefaults().matcher("/api/**");
+    RequestMatcher nonApiMatcher = new NegatedRequestMatcher(apiMatcher);
+
     return http.csrf(
             csrf ->
                 csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
@@ -25,10 +31,12 @@ public class SecurityConfig {
                     // 예외 URL
                     .requestMatchers(HttpMethod.GET, "/api/auth/csrf-token")
                     .permitAll()
+                    .requestMatchers(nonApiMatcher)
+                    .permitAll() // swagger, api-docs 대응
 
                     // 외의 것들은 인증 필요
                     .anyRequest()
-                    .authenticated())
+                    .permitAll())
         .build();
   }
 }
