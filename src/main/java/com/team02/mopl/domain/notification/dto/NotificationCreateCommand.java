6e2bee58f4d8fc7eabd1228299ea617065f4b75d
jongin-git <jongin33@gmail.com>
2026-06-24 13:10:11 +0900

    fix: 코드리뷰 후 Command Dto에 validation 추가

--- src/main/java/com/team02/mopl/domain/notification/dto/NotificationCreateCommand.java
+++ src/main/java/com/team02/mopl/domain/notification/dto/NotificationCreateCommand.java
@@ -2,11 +2,13 @@ package com.team02.mopl.domain.notification.dto;
 
 import com.team02.mopl.domain.notification.entity.enums.NotificationLevel;
 import com.team02.mopl.domain.notification.entity.enums.NotificationType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 
 public record NotificationCreateCommand(
-    UUID receiverId,
-    String title,
-    String content,
+    @NotNull UUID receiverId,
+    @NotBlank String title,
+    @NotBlank String content,
     NotificationLevel level,
-    NotificationType notificationType) {}
+    @NotNull NotificationType notificationType) {}
