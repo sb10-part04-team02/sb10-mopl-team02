b718c104fe1916aa4e8c7b6d6f1bf7854576975d
초시맥북 <fyg7575@gmail.com>
2026-06-24 09:57:17 +0900

    feat: DM 구현에 활용할 엔티티 구현

---
+++ src/main/java/com/team02/mopl/domain/dm/entity/Conversation.java
@@ -0,0 +1,16 @@
+package com.team02.mopl.domain.dm.entity;
+
+import com.team02.mopl.global.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Table(name = "conversations")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Conversation extends BaseEntity {
+
+}
---
+++ src/main/java/com/team02/mopl/domain/dm/entity/DirectMessage.java
@@ -0,0 +1,36 @@
+package com.team02.mopl.domain.dm.entity;
+
+import com.team02.mopl.domain.user.entity.User;
+import com.team02.mopl.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "direct_messages")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DirectMessage extends BaseEntity {
+
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  @JoinColumn(name = "conversation_id", columnDefinition = "uuid")
+  private Conversation conversation;
+
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  @JoinColumn(name = "sender_id", columnDefinition = "uuid")
+  private User sender;
+
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  @JoinColumn(name = "receiver_id", columnDefinition = "uuid")
+  private User receiver;
+
+  @Column(nullable = false, columnDefinition = "TEXT")
+  private String content;
+
+}
