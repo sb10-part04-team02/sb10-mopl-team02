5c3e7ace3e2566c8ebbaa86a39e697751bc577b8
초시맥북 <fyg7575@gmail.com>
2026-06-25 11:04:53 +0900

    feat: 대화 생성 컨트롤러 코드 작성

--- src/main/java/com/team02/mopl/domain/dm/controller/DirectMessageController.java
+++ src/main/java/com/team02/mopl/domain/dm/controller/DirectMessageController.java
@@ -1,7 +1,15 @@
 package com.team02.mopl.domain.dm.controller;
 
+import com.team02.mopl.domain.dm.dto.ConversationCreateRequest;
+import com.team02.mopl.domain.dm.dto.ConversationDto;
 import com.team02.mopl.domain.dm.service.DirectMessageService;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +19,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class DirectMessageController {
 
   private final DirectMessageService directMessageService;
+
+  @PostMapping
+  public ResponseEntity<ConversationDto> createConversation(
+      @RequestHeader("X-USER-ID") UUID userId,
+      @RequestBody ConversationCreateRequest request
+  ){
+    //userId는 임시
+    return ResponseEntity.ok(directMessageService.createConversation(request, userId));
+  }
 }
