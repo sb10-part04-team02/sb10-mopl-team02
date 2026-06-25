229fd859f21547f85287ff8c5dd9d282d883df37
Amperisk9 <amperisk9@gmail.com>
2026-06-25 12:13:28 +0900

    test: codeCov용 테스트코드 추가

--- src/test/java/com/team02/mopl/domain/user/service/UserServiceTest.java
+++ src/test/java/com/team02/mopl/domain/user/service/UserServiceTest.java
@@ -26,6 +26,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -60,6 +61,19 @@ class UserServiceTest {
     }
 
     @Test
+    @DisplayName("동시성 가입레이스로 DB 저장시 Unique제약조건이 위반되면 409 에러를 반환한다")
+    void fail_shouldReturn409_whenDbUniqueConstraintViolated() {
+      // given
+      UserCreateRequest request = new UserCreateRequest(name, email, password);
+      given(userRepository.existsByEmail(anyString())).willReturn(false);
+      given(userRepository.save(any(User.class)))
+          .willThrow(new DataIntegrityViolationException("Duplicate Email"));
+
+      // when & then
+      assertThrows(UserEmailDuplicateException.class, () -> userService.createUser(request));
+    }
+
+    @Test
     @DisplayName("정상적인 회원가입 요청일 때 UserDto를 반환한다")
     void success_shouldReturnUserDto_whenRequestIsValid() {
       // given
@@ -83,6 +97,26 @@ class UserServiceTest {
       User savedUser = userCaptor.getValue();
       assertThat(savedUser.getPassword())
           .isNotEqualTo(request.password()); // 암호화된 패스워드와 평문 패스워드 다름 비교
+    }
+
+    @Test
+    @DisplayName("아이디가 2글자 이하인 이메일도 정상적으로 마스킹하고 가입해서 UserDto를 반환한다")
+    void success_shouldReturnUserDto_whenEmailIsShort() {
+      // given
+      String shortEmail = "ab@gmail.com";
+      UserCreateRequest request = new UserCreateRequest(name, shortEmail, password);
+      User mockUser = new User(name, shortEmail, "encryptedPassword", null, Role.USER, false);
+      UserDto expect =
+          new UserDto(UUID.randomUUID(), Instant.now(), shortEmail, name, null, Role.USER, false);
+      given(userRepository.existsByEmail(anyString())).willReturn(false);
+      given(userRepository.save(any(User.class))).willReturn(mockUser);
+      given(userMapper.toDto(any(User.class))).willReturn(expect);
+
+      // when
+      UserDto actual = userService.createUser(request);
+
+      // then
+      assertThat(actual.email()).isEqualTo(shortEmail);
     }
   }
 }
