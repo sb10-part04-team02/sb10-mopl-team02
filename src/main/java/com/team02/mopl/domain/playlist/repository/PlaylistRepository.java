8a8f153759066e334e47a8fb7e471cc2062be17c
Lim Jiho <jiho0420@gmail.com>
2026-06-25 09:14:51 +0900

    feat: 플레이리스트 엔티티 및 리포지토리 구현 (#172)
    - Playlist: BaseMutableEntity 상속, ownerId/title/description/subscriberCount
    - PlaylistContent: playlist는 @ManyToOne 강결합, content는 contentId(UUID) 약결합
    - 플레이리스트-콘텐츠 쓰기 경로를 PlaylistContentRepository로 단일화
      (Playlist의 @OneToMany는 읽기용, cascade 미적용)
    - 스키마(01_schema_v6.sql)에 맞춰 컬럼명/타입 정렬
    - RepositoryTestSupport 기반 리포지토리 슬라이스 테스트 추가

---
+++ src/main/java/com/team02/mopl/domain/playlist/repository/PlaylistRepository.java
@@ -0,0 +1,11 @@
+package com.team02.mopl.domain.playlist.repository;
+
+import com.team02.mopl.domain.playlist.entity.Playlist;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlaylistRepository extends JpaRepository<Playlist, UUID> {
+
+  List<Playlist> findByOwnerIdAndDeletedAtIsNull(UUID ownerId);
+}
