85b47042886f72bb1e3d4e11d7f49cb486b064a1
jongin-git <jongin33@gmail.com>
2026-06-25 16:26:41 +0900

    fix: swagger 명세에 맞게 followDto 수정

--- src/main/java/com/team02/mopl/domain/follow/dto/FollowDto.java
+++ src/main/java/com/team02/mopl/domain/follow/dto/FollowDto.java
@@ -1,15 +1,12 @@
 package com.team02.mopl.domain.follow.dto;
 
 import com.team02.mopl.domain.follow.entity.Follow;
-import java.time.Instant;
 import java.util.UUID;
 
-public record FollowDto(UUID id, Instant createdAt, UUID followerId, UUID followeeId) {
+public record FollowDto(UUID id, UUID followeeId, UUID followerId) {
+
   public static FollowDto from(Follow follow) {
     return new FollowDto(
-        follow.getId(),
-        follow.getCreatedAt(),
-        follow.getFollower().getId(),
-        follow.getFollowee().getId());
+        follow.getId(), follow.getFollowee().getId(), follow.getFollower().getId());
   }
 }
