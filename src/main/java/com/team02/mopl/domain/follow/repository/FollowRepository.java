9504ae09f8a2931f0679b5920b9ed91d98bcdd49
jongin-git <jongin33@gmail.com>
2026-06-25 16:07:40 +0900

    feat: Follow 레포지토리 및 에러 추가

---
+++ src/main/java/com/team02/mopl/domain/follow/repository/FollowRepository.java
@@ -0,0 +1,22 @@
+package com.team02.mopl.domain.follow.repository;
+
+import com.team02.mopl.domain.follow.entity.Follow;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FollowRepository extends JpaRepository<Follow, UUID> {
+
+  // 중복 방지
+  boolean existByFollower_IdAndFollowee_Id(UUID followerId, UUID followeeId);
+
+  // 단건 조회
+  Optional<Follow> findByIdAndDeletedAtIsNull(UUID id);
+
+  // 본인이 특정 사용자 팔로우 중인지 조회
+  Optional<Follow> findByFollower_IdAndFollowee_IdAndDeletedAtIsNull(
+      UUID followerId, UUID followeeId);
+
+  // 특정 사용자의 팔로워 수 조회
+  long countByFollowee_IdAndDeletedAtIsNull(UUID followeeId);
+}
