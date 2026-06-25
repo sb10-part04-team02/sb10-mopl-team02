c8a0f12a318087cb28008d0203fd9900e8ff1721
SeungMinPark <68120834+raonPsm@users.noreply.github.com>
2026-06-25 13:37:37 +0900

    feat: Content DTO 및 enum 추가 (#167)
    * feat: Content DTO 및 enum 추가
    
    * fix: ReviewRepositoryTest ContentType import 경로 수정
    
    * feat: Content 요청 DTO 필드 유효성 검증 강화
    
    - ContentCreateRequest - title/description에 @Size 제한 추가, tags 요소에 @NotBlank @Size(max=20) 추가
    - ContentUpdateRequest - tags 요소에 @NotBlank @Size(max=20) 추가, 필드 포맷 정리
    
    * fix: PlaylistRepositoryTest ContentType import 경로 수정

---
+++ src/main/java/com/team02/mopl/domain/content/enums/SortBy.java
@@ -0,0 +1,7 @@
+package com.team02.mopl.domain.content.enums;
+
+public enum SortBy {
+  CREATED_AT,
+  WATCHER_COUNT,
+  RATE
+}
---
+++ src/main/java/com/team02/mopl/domain/content/enums/SortDirection.java
@@ -0,0 +1,6 @@
+package com.team02.mopl.domain.content.enums;
+
+public enum SortDirection {
+  ASCENDING,
+  DESCENDING
+}
