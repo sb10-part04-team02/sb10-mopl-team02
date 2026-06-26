9179b5e54cd9b7b8d3c8c609805bd42346688adf
초시맥북 <fyg7575@gmail.com>
2026-06-26 16:57:31 +0900

    feat: websocket 설정 추가

---
+++ src/main/java/com/team02/mopl/global/websocket/StompChannelInterceptor.java
@@ -0,0 +1,29 @@
+package com.team02.mopl.global.websocket;
+
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StompChannelInterceptor implements ChannelInterceptor {
+  @Override
+  public Message<?> preSend(Message<?> message, MessageChannel channel) {
+    StompHeaderAccessor accessor =
+        MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+
+    if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+      String sessionId = accessor.getSessionId();
+      System.out.println("새로운 STOMP 연결: " + sessionId);
+    }
+
+    if (StompCommand.DISCONNECT.equals(accessor.getCommand())) {
+      System.out.println("세션 종료 감지");
+    }
+
+    return message;
+  }
+}
