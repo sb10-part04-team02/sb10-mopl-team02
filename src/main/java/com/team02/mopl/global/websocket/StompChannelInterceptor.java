992a9d01f0927dc5b16cb950aee33951b0bb49f4
초시맥북 <fyg7575@gmail.com>
2026-06-26 17:39:48 +0900

    feat: npe 방어 코드 추가

--- src/main/java/com/team02/mopl/global/websocket/StompChannelInterceptor.java
+++ src/main/java/com/team02/mopl/global/websocket/StompChannelInterceptor.java
@@ -2,7 +2,6 @@ package com.team02.mopl.global.websocket;
 
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
@@ -12,16 +11,23 @@ import org.springframework.stereotype.Component;
 public class StompChannelInterceptor implements ChannelInterceptor {
   @Override
   public Message<?> preSend(Message<?> message, MessageChannel channel) {
+
     StompHeaderAccessor accessor =
         MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
 
-    if (StompCommand.CONNECT.equals(accessor.getCommand())) {
-      String sessionId = accessor.getSessionId();
-      System.out.println("새로운 STOMP 연결: " + sessionId);
+    if (accessor == null) {
+      return message;
     }
 
-    if (StompCommand.DISCONNECT.equals(accessor.getCommand())) {
-      System.out.println("세션 종료 감지");
+    switch (accessor.getCommand()) {
+      case CONNECT -> {
+        String sessionId = accessor.getSessionId();
+        System.out.println("새로운 STOMP 연결: " + sessionId);
+      }
+      case DISCONNECT -> System.out.println("세션 종료 감지");
+      default -> {
+        // ignore
+      }
     }
 
     return message;
