94141a7bf0d07bb3996609c1f26572314a660825
Amperisk9 <amperisk9@gmail.com>
2026-06-27 12:05:46 +0900

    fix: 리뷰 대응 - RedisConfig 변경

--- src/main/java/com/team02/mopl/global/config/RedisConfig.java
+++ src/main/java/com/team02/mopl/global/config/RedisConfig.java
@@ -1,40 +1,15 @@
 package com.team02.mopl.global.config;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectMapper.DefaultTyping;
-import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
 public class RedisConfig {
 
   @Bean
-  public RedisTemplate<String, Object> redisTemplate(
-      RedisConnectionFactory connectionFactory,
-      @Qualifier("redisSerializer") GenericJackson2JsonRedisSerializer redisSerializer) {
-    RedisTemplate<String, Object> template = new RedisTemplate<>();
-    template.setConnectionFactory(connectionFactory);
-    // 단순 문자열 직렬화용
-    template.setKeySerializer(new StringRedisSerializer());
-    template.setHashKeySerializer(new StringRedisSerializer());
-    // Object 직렬화용
-    template.setValueSerializer(redisSerializer);
-    template.setHashValueSerializer(redisSerializer);
-    return template;
-  }
-
-  @Bean("redisSerializer")
-  public GenericJackson2JsonRedisSerializer redisSerializer(ObjectMapper objectMapper) {
-    ObjectMapper redisObjectMapper = objectMapper.copy();
-    redisObjectMapper.activateDefaultTyping(
-        LaissezFaireSubTypeValidator.instance, DefaultTyping.EVERYTHING, As.PROPERTY);
-    return new GenericJackson2JsonRedisSerializer(redisObjectMapper);
+  public StringRedisTemplate redisTemplate(RedisConnectionFactory connectionFactory) {
+    return new StringRedisTemplate(connectionFactory);
   }
 }
