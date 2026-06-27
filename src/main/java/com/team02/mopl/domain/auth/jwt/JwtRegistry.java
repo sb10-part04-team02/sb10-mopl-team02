4cf27b5352850f461d52cae8f1c9836e06ff87a1
Amperisk9 <amperisk9@gmail.com>
2026-06-27 13:51:52 +0900

    fix: JwtRegistry의 RedisTemplate 변경

--- src/main/java/com/team02/mopl/domain/auth/jwt/JwtRegistry.java
+++ src/main/java/com/team02/mopl/domain/auth/jwt/JwtRegistry.java
@@ -1,7 +1,7 @@
 package com.team02.mopl.domain.auth.jwt;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,5 +9,5 @@ import org.springframework.stereotype.Component;
 public class JwtRegistry {
 
   private final JwtTokenProvider jwtTokenProvider;
-  private final RedisTemplate<String, Object> redisTemplate;
+  private final StringRedisTemplate redisTemplate;
 }
