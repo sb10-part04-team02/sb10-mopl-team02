94d3e4d22e68173b1b23e3a07bcbea0feaf1068b
jongin-git <jongin33@gmail.com>
2026-07-01 09:14:47 +0900

    fix: notificationRepository 불필요한 메서드 삭제

--- src/main/java/com/team02/mopl/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/team02/mopl/domain/notification/repository/NotificationRepository.java
@@ -1,15 +1,12 @@
 package com.team02.mopl.domain.notification.repository;
 
 import com.team02.mopl.domain.notification.entity.Notification;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository
     extends JpaRepository<Notification, UUID>, NotificationRepositoryCustom {
-
-  List<Notification> findByReceiver_IdAndDeletedAtIsNullOrderByCreatedAtDescIdDesc(UUID receiverId);
 
   Optional<Notification> findByIdAndDeletedAtIsNull(UUID notificationId);
 
