b2d153cb7a7e19a8c8b56467d14f89b8f9cdd26b
Amperisk9 <amperisk9@gmail.com>
2026-07-05 12:05:07 +0900

    feat: 유저 목록 조회 API 레포지토리 구현 (테스트 완료)

---
+++ src/main/java/com/team02/mopl/domain/user/repository/UserRepositoryImpl.java
@@ -0,0 +1,142 @@
+package com.team02.mopl.domain.user.repository;
+
+import com.querydsl.core.types.Expression;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.team02.mopl.domain.user.entity.QUser;
+import com.team02.mopl.domain.user.entity.User;
+import com.team02.mopl.domain.user.entity.enums.Role;
+import com.team02.mopl.domain.user.enums.UserSortBy;
+import com.team02.mopl.global.enums.SortDirection;
+import com.team02.mopl.global.exception.BusinessException;
+import com.team02.mopl.global.exception.ErrorCode;
+import jakarta.persistence.EntityManager;
+import java.time.Instant;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import org.hibernate.jpa.HibernateHints;
+import org.springframework.util.StringUtils;
+
+public class UserRepositoryImpl implements UserRepositoryCustom {
+
+  private static final QUser user = QUser.user;
+  private final JPAQueryFactory queryFactory;
+
+  public UserRepositoryImpl(EntityManager em) {
+    this.queryFactory = new JPAQueryFactory(em);
+  }
+
+  @Override
+  public List<User> findUsersByCursor(
+      String emailLike,
+      Role roleEqual,
+      Boolean isLocked,
+      String cursor,
+      UUID idAfter,
+      Integer limit,
+      SortDirection sortDirection,
+      UserSortBy sortBy) {
+
+    boolean ascending = sortDirection == SortDirection.ASCENDING;
+    boolean firstPage = cursor == null && idAfter == null;
+    if (cursor == null ^ idAfter == null) { // XOR: 둘 중 하나만 있을 경우
+      throw new BusinessException(ErrorCode.INVALID_REQUEST);
+    }
+
+    return queryFactory
+        .selectFrom(user)
+        .where(
+            user.deletedAt.isNull(),
+            emailLikeEq(emailLike),
+            roleEq(roleEqual),
+            isLockedEq(isLocked),
+            firstPage ? null : cursorPredicate(sortBy, ascending, cursor, idAfter))
+        .orderBy(orderSpecifiers(sortBy, ascending))
+        // 조회전용 영속성 스냅샷 생성 생략
+        .setHint(HibernateHints.HINT_READ_ONLY, true)
+        .limit(limit)
+        .fetch();
+  }
+
+  private BooleanExpression emailLikeEq(String emailLike) {
+    return StringUtils.hasText(emailLike) ? user.email.contains(emailLike) : null;
+  }
+
+  private BooleanExpression roleEq(Role roleEqual) {
+    return roleEqual != null ? user.role.eq(roleEqual) : null;
+  }
+
+  private BooleanExpression isLockedEq(Boolean isLocked) {
+    return isLocked != null ? user.isLocked.eq(isLocked) : null;
+  }
+
+  private BooleanExpression cursorPredicate(
+      UserSortBy sortBy, boolean ascending, String cursor, UUID idAfter) {
+    record CursorTuple(Expression<?> sortKey, Object cursorValue) {}
+
+    CursorTuple tuple =
+        switch (sortBy) {
+          case NAME -> new CursorTuple(user.name, cursor);
+          case EMAIL -> new CursorTuple(user.email, cursor);
+          case CREATED_AT -> new CursorTuple(user.createdAt, parseInstantCursor(cursor));
+          case IS_LOCKED -> new CursorTuple(user.isLocked, parseBooleanCursor(cursor));
+          case ROLE -> new CursorTuple(user.role, parseRoleCursor(cursor).name());
+        };
+
+    String op = ascending ? ">" : "<";
+    return Expressions.booleanTemplate(
+        "({0}, {1}) " + op + " ({2}, {3})",
+        tuple.sortKey,
+        user.id,
+        Expressions.constant(tuple.cursorValue),
+        Expressions.constant(idAfter));
+  }
+
+  private OrderSpecifier<?>[] orderSpecifiers(UserSortBy sortBy, boolean ascending) {
+    List<OrderSpecifier<?>> orders = new ArrayList<>();
+    Order order = ascending ? Order.ASC : Order.DESC;
+
+    // 1차 정렬
+    OrderSpecifier<?> targetOrder =
+        switch (sortBy) {
+          case NAME -> new OrderSpecifier<>(order, user.name);
+          case EMAIL -> new OrderSpecifier<>(order, user.email);
+          case CREATED_AT -> new OrderSpecifier<>(order, user.createdAt);
+          case IS_LOCKED -> new OrderSpecifier<>(order, user.isLocked);
+          case ROLE -> new OrderSpecifier<>(order, user.role);
+        };
+
+    orders.add(targetOrder);
+    orders.add(new OrderSpecifier<>(order, user.id)); // 2차정렬은 id
+
+    return orders.toArray(OrderSpecifier[]::new);
+  }
+
+  private Instant parseInstantCursor(String cursor) {
+    try {
+      return Instant.parse(cursor);
+    } catch (DateTimeParseException e) {
+      throw new BusinessException(ErrorCode.INVALID_REQUEST);
+    }
+  }
+
+  private Boolean parseBooleanCursor(String cursor) {
+    if (!"true".equalsIgnoreCase(cursor) && !"false".equalsIgnoreCase(cursor)) {
+      throw new BusinessException(ErrorCode.INVALID_REQUEST);
+    }
+    return Boolean.parseBoolean(cursor);
+  }
+
+  private Role parseRoleCursor(String cursor) {
+    try {
+      return Role.valueOf(cursor.toUpperCase());
+    } catch (IllegalArgumentException e) {
+      throw new BusinessException(ErrorCode.INVALID_REQUEST);
+    }
+  }
+}
---
+++ src/test/java/com/team02/mopl/domain/user/repository/UserRepositoryTest.java
@@ -0,0 +1,359 @@
+package com.team02.mopl.domain.user.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.BDDAssertions.tuple;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.team02.mopl.domain.user.entity.User;
+import com.team02.mopl.domain.user.entity.enums.Role;
+import com.team02.mopl.domain.user.enums.UserSortBy;
+import com.team02.mopl.global.enums.SortDirection;
+import com.team02.mopl.global.exception.BusinessException;
+import com.team02.mopl.support.RepositoryTestSupport;
+import jakarta.persistence.EntityManager;
+import java.util.Comparator;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class UserRepositoryTest extends RepositoryTestSupport {
+
+  @Autowired private UserRepository userRepository;
+  @Autowired private EntityManager em;
+
+  @Test
+  @DisplayName("필수 파라미터만으로 조회할경우 유저 전체를 조회한다")
+  void success_shouldRetrieveAllUsers_whenOnlyRequiredParametersAreProvided() {
+    // given
+    saveUser("user1", "user1@gmail.com", Role.USER, false);
+    saveUser("user2", "user2@naver.com", Role.USER, true);
+    saveUser("user3", "user3@gmail.com", Role.ADMIN, false);
+
+    // when
+    List<User> result =
+        userRepository.findUsersByCursor(
+            null, null, null, null, null, 5, SortDirection.ASCENDING, UserSortBy.NAME);
+
+    // then
+    assertThat(result).hasSize(3);
+  }
+
+  @Test
+  @DisplayName("limit가 전체 user 수보다 적으면 limit 만큼만 유저를 반환한다")
+  void success_shouldReturnExactlyLimitSize_whenLimitIsLessThanTotalUserCount() {
+    // given
+    saveUser("user1", "user1@gmail.com", Role.USER, false);
+    saveUser("user2", "user2@naver.com", Role.USER, true);
+    saveUser("user3", "user3@gmail.com", Role.ADMIN, false);
+    int limit = 2;
+
+    // when
+    List<User> result =
+        userRepository.findUsersByCursor(
+            null, null, false, null, null, limit, SortDirection.ASCENDING, UserSortBy.NAME);
+
+    // then
+    assertThat(result.size()).isEqualTo(limit);
+  }
+
+  @Test
+  @DisplayName("검색옵션이 제공되면 해당하는 유저를 찾아 반환한다")
+  void success_shouldReturnMatchingUsers_whenSearchOptionsAreProvided() {
+    // given
+    saveUser("user1", "user1@example.com", Role.USER, false);
+    saveUser("user2", "user2@gmail.com", Role.ADMIN, false);
+    saveUser("user3", "user3@example.com", Role.ADMIN, true);
+    String emailLike = "example";
+    Role role = Role.ADMIN;
+    boolean isLocked = true;
+
+    // when
+    List<User> result =
+        userRepository.findUsersByCursor(
+            emailLike, role, isLocked, null, null, 10, SortDirection.ASCENDING, UserSortBy.NAME);
+
+    // then
+    assertThat(result)
+        .hasSize(1)
+        .first()
+        .satisfies(
+            user -> {
+              assertThat(user.getName()).isEqualTo("user3");
+              assertThat(user.getEmail()).contains(emailLike);
+              assertThat(user.getRole()).isEqualTo(role);
+              assertThat(user.isLocked()).isEqualTo(isLocked);
+            });
+  }
+
+  @Test
+  @DisplayName("이름을 커서로 페이징할때 정렬 순서에 맞춰 유저를 반환한다")
+  void success_shouldReturnUsersInOrder_whenPagingByNameCursor() {
+    // given
+    SortDirection direction = SortDirection.ASCENDING;
+    UserSortBy sortBy = UserSortBy.NAME;
+
+    saveUser("A_user", "A0@gmail.com", Role.USER, false);
+    saveUser("B_user", "B1@gmail.com", Role.USER, false);
+    saveUser("B_user", "B2@gmail.com", Role.USER, false);
+    saveUser("C_user", "C@gmail.com", Role.USER, false);
+    List<User> totalUser =
+        userRepository.findUsersByCursor(null, null, null, null, null, 10, direction, sortBy);
+
+    User cursorUser = totalUser.get(1);
+    User user1 = totalUser.get(2);
+    User user2 = totalUser.get(3);
+
+    // when
+    List<User> result =
+        userRepository.findUsersByCursor(
+            null, null, null, cursorUser.getName(), cursorUser.getId(), 10, direction, sortBy);
+
+    // then
+    assertThat(cursorUser.getName()).isEqualTo(user1.getName());
+    assertThat(result)
+        .hasSize(2)
+        .extracting(User::getName, User::getId)
+        .containsExactly( // user1 < user2 순서 확인 포함
+            tuple(user1.getName(), user1.getId()), tuple(user2.getName(), user2.getId()));
+  }
+
+  @Test
+  @DisplayName("이메일을 커서로 페이징할때 정렬 순서에 맞춰 유저를 반환한다")
+  void success_shouldReturnUsersInOrder_whenPagingByEmailCursor() {
+    // given
+    SortDirection direction = SortDirection.ASCENDING;
+    UserSortBy sortBy = UserSortBy.EMAIL; // email은 unique 속성
+
+    saveUser("A_user", "A@gmail.com", Role.USER, false);
+    saveUser("B_user", "B@gmail.com", Role.USER, false);
+    saveUser("C_user", "C@gmail.com", Role.USER, false);
+    saveUser("D_user", "D@gmail.com", Role.USER, false);
+    List<User> totalUser =
+        userRepository.findUsersByCursor(null, null, null, null, null, 10, direction, sortBy);
+
+    User cursorUser = totalUser.get(1);
+    User user1 = totalUser.get(2);
+    User user2 = totalUser.get(3);
+
+    // when
+    List<User> result =
+        userRepository.findUsersByCursor(
+            null, null, null, cursorUser.getEmail(), cursorUser.getId(), 10, direction, sortBy);
+
+    // then
+    assertThat(cursorUser.getEmail()).isNotEqualTo(user1.getEmail());
+    assertThat(result)
+        .hasSize(2)
+        .extracting(User::getEmail, User::getId)
+        .containsExactly( // user1 < user2 순서 확인 포함
+            tuple(user1.getEmail(), user1.getId()), tuple(user2.getEmail(), user2.getId()));
+  }
+
+  @Test
+  @DisplayName("생성시간을 커서로 페이징할때 정렬 순서에 맞춰 유저를 반환한다")
+  void success_shouldReturnUsersInOrder_whenPagingByCreatedAtCursor() {
+    // given
+    SortDirection direction = SortDirection.DESCENDING; // 테스트 거버리지용 역
+    UserSortBy sortBy = UserSortBy.CREATED_AT;
+
+    saveUser("A_user", "A@gmail.com", Role.USER, false);
+    saveUser("B_user", "B@gmail.com", Role.USER, false);
+    saveUser("C_user", "C@gmail.com", Role.USER, false);
+    saveUser("D_user", "D@gmail.com", Role.USER, false);
+    List<User> totalUser =
+        userRepository.findUsersByCursor(null, null, null, null, null, 10, direction, sortBy);
+
+    List<User> sortedUser =
+        totalUser.stream()
+            .sorted(
+                Comparator
+                    // 1차 정렬기준(createdAt)
+                    .comparing(User::getCreatedAt)
+                    .reversed()
+                    // 2차 정렬기준(id. DB와 같은 Unsigned 방식. 내림차순 정렬)
+                    .thenComparing(User::getId, DB_UUID_COMPARATOR.reversed()))
+            .toList();
+
+    User cursorUser = sortedUser.get(1);
+    User user1 = sortedUser.get(2);
+    User user2 = sortedUser.get(3);
+
+    // when
+    List<User> result =
+        userRepository.findUsersByCursor(
+            null,
+            null,
+            null,
+            cursorUser.getCreatedAt().toString(),
+            cursorUser.getId(),
+            10,
+            direction,
+            sortBy);
+
+    // then
+    assertThat(result)
+        .hasSize(2)
+        .extracting(User::getCreatedAt, User::getId)
+        .containsExactly( // user1 < user2 순서 확인 포함
+            tuple(user1.getCreatedAt(), user1.getId()), tuple(user2.getCreatedAt(), user2.getId()));
+  }
+
+  @Test
+  @DisplayName("잠금여부를 커서로 페이징할때 정렬 순서에 맞춰 유저를 반환한다")
+  void success_shouldReturnUsersInOrder_whenPagingByIsLockedCursor() {
+    // given
+    SortDirection direction = SortDirection.ASCENDING;
+    UserSortBy sortBy = UserSortBy.IS_LOCKED;
+
+    saveUser("A_user", "A@gmail.com", Role.USER, false);
+    saveUser("B_user", "B@gmail.com", Role.USER, true);
+    saveUser("C_user", "C@gmail.com", Role.USER, false);
+    saveUser("D_user", "D@gmail.com", Role.USER, false);
+    List<User> totalUser =
+        userRepository.findUsersByCursor(null, null, null, null, null, 10, direction, sortBy);
+
+    // 잠금여부 오름차순일땐 false -> true
+    List<User> sortedUser =
+        totalUser.stream()
+            .sorted(
+                Comparator
+                    // 1차 정렬기준(isLocked)
+                    .comparing(User::isLocked)
+                    // 2차 정렬기준(id. DB와 같은 Unsigned 방식 오름차순 정렬)
+                    .thenComparing(User::getId, DB_UUID_COMPARATOR))
+            .toList();
+
+    User cursorUser = sortedUser.get(1);
+    User user1 = sortedUser.get(2);
+    User user2 = sortedUser.get(3);
+
+    // when
+    List<User> result =
+        userRepository.findUsersByCursor(
+            null,
+            null,
+            null,
+            Boolean.toString(cursorUser.isLocked()),
+            cursorUser.getId(),
+            10,
+            direction,
+            sortBy);
+
+    // then
+    assertThat(result)
+        .hasSize(2)
+        .extracting(User::isLocked, User::getId)
+        .containsExactly( // user1 < user2 순서 확인 포함
+            tuple(user1.isLocked(), user1.getId()), tuple(user2.isLocked(), user2.getId()));
+  }
+
+  @Test
+  @DisplayName("권한을 커서로 페이징할때 정렬 순서에 맞춰 유저를 반환한다")
+  void success_shouldReturnUsersInOrder_whenPagingByRoleCursor() {
+    // given
+    SortDirection direction = SortDirection.ASCENDING;
+    UserSortBy sortBy = UserSortBy.ROLE;
+
+    saveUser("A_user", "A@gmail.com", Role.USER, false);
+    saveUser("B_user", "B@gmail.com", Role.USER, true);
+    saveUser("C_user", "C@gmail.com", Role.ADMIN, false);
+    saveUser("D_user", "D@gmail.com", Role.USER, false);
+    List<User> totalUser =
+        userRepository.findUsersByCursor(null, null, null, null, null, 10, direction, sortBy);
+
+    // 권한 오름차순일땐 USER -> ADMIN
+    List<User> sortedUser =
+        totalUser.stream()
+            .sorted(
+                Comparator
+                    // 1차 정렬기준(Role)
+                    .comparing((User u) -> u.getRole().name())
+                    // 2차 정렬기준(id. DB와 같은 Unsigned 방식 오름차순 정렬)
+                    .thenComparing(User::getId, DB_UUID_COMPARATOR))
+            .toList();
+
+    User cursorUser = sortedUser.get(1);
+    User user1 = sortedUser.get(2);
+    User user2 = sortedUser.get(3);
+
+    // when
+    List<User> result =
+        userRepository.findUsersByCursor(
+            null,
+            null,
+            null,
+            cursorUser.getRole().name(),
+            cursorUser.getId(),
+            10,
+            direction,
+            sortBy);
+
+    // then
+    assertThat(result)
+        .hasSize(2)
+        .extracting(User::getRole, User::getId)
+        .containsExactly( // user1 < user2 순서 확인 포함
+            tuple(user1.getRole(), user1.getId()), tuple(user2.getRole(), user2.getId()));
+  }
+
+  private static Stream<Arguments> provideCursorOrIdAfterMissing() {
+    return Stream.of(
+        Arguments.of(null, UUID.randomUUID(), "cursor 누락"),
+        Arguments.of("cursor", null, "idAfter 누락"));
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideCursorOrIdAfterMissing")
+  @DisplayName("첫 페이지 조회가 아닐때 커서와 idAfter 둘 중 하나만 누락되면 예외를 던진다")
+  void fail_shouldThrowException_whenCursorOrIdAfterIsMissing(
+      String cursor, UUID idAfter, String description) {
+    // when & then
+    assertThrows(
+        BusinessException.class,
+        () ->
+            userRepository.findUsersByCursor(
+                null, null, null, cursor, idAfter, 10, SortDirection.DESCENDING, UserSortBy.NAME));
+  }
+
+  private static Stream<Arguments> provideInvalidCursor() {
+    return Stream.of(
+        Arguments.of("invalid instant time", UserSortBy.CREATED_AT, "효력없는 createdAt"),
+        Arguments.of("invalid boolean", UserSortBy.IS_LOCKED, "효력없는 isLocked"),
+        Arguments.of("invalid role", UserSortBy.ROLE, "효력없는 Role"));
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideInvalidCursor")
+  @DisplayName("정렬을 파싱하는데 실패한다면 예외를 던진다")
+  void fail_shouldThrowException_whenUserSortByParsingFails(
+      String cursor, UserSortBy sortBy, String description) {
+    // when & then
+    assertThrows(
+        BusinessException.class,
+        () ->
+            userRepository.findUsersByCursor(
+                null, null, null, cursor, UUID.randomUUID(), 1, SortDirection.DESCENDING, sortBy));
+  }
+
+  private User saveUser(String name, String email, Role role, boolean isLocked) {
+    User user = new User(name, email, "pwd", null, role, isLocked);
+    em.persist(user); // 영속화
+    return user;
+  }
+
+  private static final Comparator<UUID> DB_UUID_COMPARATOR =
+      (id1, id2) -> {
+        int mostSig =
+            Long.compareUnsigned(id1.getMostSignificantBits(), id2.getMostSignificantBits());
+        if (mostSig != 0) {
+          return mostSig;
+        }
+        return Long.compareUnsigned(id1.getLeastSignificantBits(), id2.getLeastSignificantBits());
+      };
+}
