a70666d2763243d152fb689a44f8bfa2075f269e
jongin-git <jongin33@gmail.com>
2026-07-13 20:18:31 +0900

    feat: Redis Pub/Sub 알림 SSE fan-out 구현

--- src/main/java/com/team02/mopl/global/config/RedisConfig.java
+++ src/main/java/com/team02/mopl/global/config/RedisConfig.java
@@ -1,9 +1,13 @@
 package com.team02.mopl.global.config;
 
+import com.team02.mopl.domain.notification.redis.NotificationRedisChannels;
+import com.team02.mopl.domain.notification.redis.NotificationSseFanOutSubscriber;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 
 @Configuration
 public class RedisConfig {
@@ -11,5 +15,17 @@ public class RedisConfig {
   @Bean
   public StringRedisTemplate redisTemplate(RedisConnectionFactory connectionFactory) {
     return new StringRedisTemplate(connectionFactory);
+  }
+
+  @Bean
+  public RedisMessageListenerContainer redisMessageListenerContainer(
+      RedisConnectionFactory connectionFactory,
+      NotificationSseFanOutSubscriber notificationSseFanOutSubscriber) {
+    RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+    container.setConnectionFactory(connectionFactory);
+    container.addMessageListener(
+        notificationSseFanOutSubscriber,
+        new ChannelTopic(NotificationRedisChannels.NOTIFICATION_SSE_FAN_OUT));
+    return container;
   }
 }
